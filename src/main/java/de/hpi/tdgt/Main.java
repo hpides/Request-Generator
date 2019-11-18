615783c82bd476b3d52d0a258c697909c4ee17c8
Eric Ackermann <eric.ackermann@student.hpi.uni-potsdam.de>
2019-11-18 15:38:39 +0100

    REST client implemented

--- src/main/java/de/hpi/tdgt/Main.java
+++ src/main/java/de/hpi/tdgt/Main.java
@@ -1,30 +1,73 @@
 package de.hpi.tdgt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.hpi.tdgt.deserialisation.Deserializer;
+import de.hpi.tdgt.requesthandling.RestClient;
+import lombok.val;
 import org.apache.commons.io.IOUtils;
 import de.hpi.tdgt.test.Test;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-
+import java.net.URL;
+import java.util.HashMap;
 public class Main {
-    public static void main(String[] args) {
+    public static final String USERNAME="superuser";
+    public static final String PASSWORD="somepw";
+    public static void main(String[] args) throws IOException {
         if(args.length == 0){
             try {
-                System.err.println("Usage: java -jar "+new java.io.File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getName()+" <Path to request JSON>");
+                System.err.println("Usage: java -jar "+new java.io.File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getName()+" load <Path to request JSON>");
+                System.err.println("or: java -jar "+new java.io.File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getName()+" testRest");
                 System.exit(1);
             } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
         }
-        try {
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(new FileInputStream(args[0]), writer);
-            String json = writer.toString();
-            Test deserializedTest = Deserializer.deserialize(json);
-            System.out.println("Successfully deserialized input json including "+deserializedTest.getStories().length+" stories.");
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(args[0].equals("load") ) {
+            try {
+                StringWriter writer = new StringWriter();
+                IOUtils.copy(new FileInputStream(args[0]), writer);
+                String json = writer.toString();
+                Test deserializedTest = Deserializer.deserialize(json);
+                System.out.println("Successfully deserialized input json including " + deserializedTest.getStories().length + " stories.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else{
+            val rc = new RestClient();
+            val params = new HashMap<String, String>();
+            params.put("username", USERNAME);
+            params.put("password", PASSWORD);
+
+            System.out.println("--- Testing user creation and update ---");
+            var result = rc.postBodyToEndpoint(new URL("http://users/users/new"),new ObjectMapper().writeValueAsString(params));
+            System.out.println("Create user: "+result.toString()+" and code: "+result.getReturnCode()+" in: "+result.durationMillis()+" ms.");
+            result = rc.getFromEndpointWithAuth(new URL("http://users/users/all"),null, USERNAME, PASSWORD);
+            System.out.println("Get all users: "+result.toString()+" and code: "+result.getReturnCode()+" in: "+result.durationMillis()+" ms.");
+            result = rc.putFormToEndpointWithAuth(new URL("http://users/users/update"),params, USERNAME, PASSWORD);
+            System.out.println("Update user: "+result.toString()+" and code: "+result.getReturnCode()+" in: "+result.durationMillis()+" ms.");
+
+            System.out.println("--- Testing post creation ---");
+            params.clear();
+            params.put("title","A very good post");
+            params.put("text", "because it is rather short.");
+            result = rc.postFormToEndpointWithAuth(new URL("http://posts/posts/new"),params, USERNAME, PASSWORD);
+            System.out.println("Create post: "+result.toString()+" and code: "+result.getReturnCode()+" in: "+result.durationMillis()+" ms.");
+            result = rc.getFromEndpointWithAuth(new URL("http://posts/posts/all"),null, USERNAME, PASSWORD);
+            System.out.println("Get all posts: "+result.toString()+" and code: "+result.getReturnCode()+" in: "+result.durationMillis()+" ms.");
+
+
+            System.out.println("--- Testing search ---");
+            params.clear();
+            params.put("key","short");
+            result = rc.getFromEndpointWithAuth(new URL("http://search/posts/search"),params,USERNAME,PASSWORD);
+            System.out.println("Search: "+result.toString()+" and code: "+result.getReturnCode()+" in: "+result.durationMillis()+" ms.");
+
+            System.out.println("--- Deleting user ---");
+            result = rc.deleteFromEndpointWithAuth(new URL("http://users/users/delete"),null, USERNAME, PASSWORD);
+            System.out.println("Delete user: "+result.toString()+" and code: "+result.getReturnCode()+" in: "+result.durationMillis()+" ms.");
         }
     }
 }
---
+++ src/test/java/de/hpi/tdgt/requesthandling/TestRequestHandling.java
@@ -0,0 +1,174 @@
+package de.hpi.tdgt.requesthandling;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import lombok.val;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestRequestHandling {
+
+    //Based on https://www.codeproject.com/tips/1040097/create-a-simple-web-server-in-java-http-server
+    @BeforeAll
+    public void launchTestServer() throws IOException {
+        int port = 9000;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        System.out.println("server started at " + port);
+        server.createContext("/", new HttpHandlers.GetHandler());
+        server.createContext("/jsonObject", new HttpHandlers.JSONObjectGetHandler());
+        server.createContext("/jsonArray", new HttpHandlers.JSONArrayGetHandler());
+        server.createContext("/echoPost", new HttpHandlers.PostHandler());
+        server.createContext("/postWithBody", new HttpHandlers.PostBodyHandler());
+        server.createContext("/putWithBody", new HttpHandlers.PostBodyHandler());
+        server.createContext("/auth", new HttpHandlers.AuthHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+    @Test
+    public void testSimpleRequest() throws IOException {
+        val rc = new RestClient();
+        val result = rc.getFromEndpoint(new URL("http://localhost:9000/"), new HashMap<>());
+        String response = new String(result.getResponse(),StandardCharsets.UTF_8);
+        assertThat(response, equalTo("Welcome!\n"));
+    }
+
+    @Test
+    public void testContentType() throws IOException {
+        val rc = new RestClient();
+        val result = rc.getFromEndpoint(new URL("http://localhost:9000/"), new HashMap<>());
+        assertThat(result.isPlainText(), is(true));
+    }
+    @Test
+    public void testContentDecoding() throws IOException {
+        val rc = new RestClient();
+        val result = rc.getFromEndpoint(new URL("http://localhost:9000/"), new HashMap<>());
+        assertThat(result.toString(), equalTo("Welcome!\n"));
+    }
+
+    @Test
+    public void testGETParams() throws IOException {
+        val rc = new RestClient();
+        val params = new HashMap<String,String>();
+        params.put("param","value");
+        val result = rc.getFromEndpoint(new URL("http://localhost:9000/"), params);
+        assertThat(result.toString(), stringContainsInOrder("param","value"));
+    }
+
+    @Test
+    public void testDELETEParams() throws IOException {
+        val rc = new RestClient();
+        val params = new HashMap<String,String>();
+        params.put("param","value");
+        val result = rc.deleteFromEndpoint(new URL("http://localhost:9000/"), params);
+        assertThat(result.toString(), stringContainsInOrder("param","value"));
+    }
+
+    @Test
+    public void testJSON() throws IOException {
+        val rc = new RestClient();
+        val params = new HashMap<String,String>();
+        params.put("param","value");
+        val result = rc.getFromEndpoint(new URL("http://localhost:9000/jsonObject"), params);
+        assertThat(result.toJson().isObject(),is(true));
+    }
+    @Test
+    public void testJSONArray() throws IOException {
+        val rc = new RestClient();
+        val params = new HashMap<String,String>();
+        params.put("param","value");
+        val result = rc.getFromEndpoint(new URL("http://localhost:9000/jsonArray"), params);
+        assertThat(result.toJson().isArray(),is(true));
+    }
+
+    @Test
+    public void testMeasuresTime() throws IOException {
+        val rc = new RestClient();
+        val params = new HashMap<String,String>();
+        params.put("param","value");
+        val result = rc.getFromEndpoint(new URL("http://localhost:9000/jsonArray"), params);
+        assertThat(result.durationMillis(), is(both(greaterThan(0L)).and(lessThan(100000L))));
+    }
+
+    @Test
+    public void testPOSTFormParams() throws IOException {
+        val rc = new RestClient();
+        val params = new HashMap<String,String>();
+        params.put("param","value");
+        val result = rc.postFormToEndpoint(new URL("http://localhost:9000/echoPost"), params);
+        assertThat(result.toString(), stringContainsInOrder("param = value"));
+    }
+    @Test
+    public void testPUTFormParams() throws IOException {
+        val rc = new RestClient();
+        val params = new HashMap<String,String>();
+        params.put("param","value");
+        val result = rc.putFormToEndpoint(new URL("http://localhost:9000/echoPost"), params);
+        assertThat(result.toString(), stringContainsInOrder("param = value"));
+    }
+
+    @Test
+    public void testPOSTBodyParams() throws IOException {
+        val rc = new RestClient();
+        val body = "{\"param\":\"value\"}";
+        val result = rc.postBodyToEndpoint(new URL("http://localhost:9000/postWithBody"), body);
+        assertThat(result.toString(), equalTo(body));
+    }
+    @Test
+    public void testPUTBodyParams() throws IOException {
+        val rc = new RestClient();
+        val body = "{\"param\":\"value\"}";
+        val result = rc.putBodyToEndpoint(new URL("http://localhost:9000/postWithBody"), body);
+        assertThat(result.toString(), equalTo(body));
+    }
+
+    @Test
+    public void testGETWithAuth() throws IOException {
+        val rc = new RestClient();
+        val result = rc.getFromEndpointWithAuth(new URL("http://localhost:9000/auth"),null,HttpHandlers.AuthHandler.username, HttpHandlers.AuthHandler.password);
+        assertThat(result.getReturnCode(), equalTo(200));
+    }
+
+    @Test
+    public void testDELETEWithAuth() throws IOException {
+        val rc = new RestClient();
+        val result = rc.deleteFromEndpointWithAuth(new URL("http://localhost:9000/auth"),null,HttpHandlers.AuthHandler.username, HttpHandlers.AuthHandler.password);
+        assertThat(result.getReturnCode(), equalTo(200));
+    }
+    @Test
+    public void testPOSTBodyWithAuth() throws IOException {
+        val rc = new RestClient();
+        val result = rc.postBodyToEndpointWithAuth(new URL("http://localhost:9000/auth"),"\"Something\"",HttpHandlers.AuthHandler.username, HttpHandlers.AuthHandler.password);
+        assertThat(result.getReturnCode(), equalTo(200));
+    }
+    @Test
+    public void testPOSTFormWithAuth() throws IOException {
+        val rc = new RestClient();
+        val result = rc.postFormToEndpointWithAuth(new URL("http://localhost:9000/auth"),new HashMap<>(),HttpHandlers.AuthHandler.username, HttpHandlers.AuthHandler.password);
+        assertThat(result.getReturnCode(), equalTo(200));
+    }
+    @Test
+    public void testPUTBodyWithAuth() throws IOException {
+        val rc = new RestClient();
+        val result = rc.putBodyToEndpointWithAuth(new URL("http://localhost:9000/auth"),"\"Something\"",HttpHandlers.AuthHandler.username, HttpHandlers.AuthHandler.password);
+        assertThat(result.getReturnCode(), equalTo(200));
+    }
+    @Test
+    public void testPUTFormWithAuth() throws IOException {
+        val rc = new RestClient();
+        val result = rc.putFormToEndpointWithAuth(new URL("http://localhost:9000/auth"),new HashMap<>(),HttpHandlers.AuthHandler.username, HttpHandlers.AuthHandler.password);
+        assertThat(result.getReturnCode(), equalTo(200));
+    }
+
+
+}
