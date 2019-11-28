56f79205ad10eaf2d476a516365f33e78628da53
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-11-28 11:18:57 +0100

    Refactoring: Time_Measurement inherits server now.

--- src/test/java/de/hpi/tdgt/time_measurement/TimeStorageTest.java
+++ src/test/java/de/hpi/tdgt/time_measurement/TimeStorageTest.java
@@ -1,28 +1,18 @@
 package de.hpi.tdgt.time_measurement;
 
-import com.sun.net.httpserver.HttpServer;
 import de.hpi.tdgt.RequestHandlingFramework;
 import de.hpi.tdgt.Utils;
 import de.hpi.tdgt.deserialisation.Deserializer;
-import de.hpi.tdgt.requesthandling.HttpHandlers;
 import de.hpi.tdgt.test.story.UserStory;
-import de.hpi.tdgt.test.story.activity.Data_Generation;
 import de.hpi.tdgt.test.story.activity.Request;
 import de.hpi.tdgt.test.time_measurement.TimeStorage;
 import lombok.extern.log4j.Log4j2;
 import lombok.val;
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.InetSocketAddress;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.greaterThan;
 @Log4j2
 public class TimeStorageTest extends RequestHandlingFramework {
