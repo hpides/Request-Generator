2d473f3e853cd99e42cff8c3c9e3cfb36df3a7c5
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-12-02 19:12:56 +0100

    Fixed clone for start.

---
+++ src/test/java/de/hpi/tdgt/atom/StartTest.java
@@ -0,0 +1,30 @@
+package de.hpi.tdgt.atom;
+
+import de.hpi.tdgt.Utils;
+import de.hpi.tdgt.deserialisation.Deserializer;
+import de.hpi.tdgt.test.story.atom.Atom;
+import de.hpi.tdgt.test.story.atom.Start;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+public class StartTest {
+
+    private Atom firstAtomOfFirstStory;
+
+    @BeforeEach
+    public void prepareTest() throws IOException {
+        firstAtomOfFirstStory = Deserializer.deserialize(new Utils().getExampleJSON()).getStories()[0].getAtoms()[0];
+    }
+    @Test
+    public void setFirstAtomOfFirstStoryIsStart(){
+        assertNotSame(firstAtomOfFirstStory,firstAtomOfFirstStory.clone());
+    }
+
+}
