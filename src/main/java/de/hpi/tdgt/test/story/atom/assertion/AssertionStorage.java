662bdeaed596fb7098ec5ac582ae8ca396057fd5
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-12-09 15:43:32 +0100

    Create mqtt thread on demand.

--- src/main/java/de/hpi/tdgt/test/story/atom/assertion/AssertionStorage.java
+++ src/main/java/de/hpi/tdgt/test/story/atom/assertion/AssertionStorage.java
@@ -91,8 +91,6 @@ public class AssertionStorage {
                 e.printStackTrace();
             }
         };
-        reporter = new Thread(mqttRunnable);
-        reporter.start();
     }
 
     @Getter
