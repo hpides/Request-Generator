7382334e85bd59e628fd6b1c3eb4d19be262d2dd
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-12-11 10:21:35 +0100

    Add times synchronously in TimeStorageStreamsAllTimesOfAllStoriesUsingMQTT.

--- src/test/java/de/hpi/tdgt/mqtt/MQTTTest.java
+++ src/test/java/de/hpi/tdgt/mqtt/MQTTTest.java
@@ -71,6 +71,8 @@ public class MQTTTest extends RequestHandlingFramework {
     }
     @Test
     public void TimeStorageStreamsAllTimesOfAllStoriesUsingMQTT() throws MqttException, InterruptedException, IOException {
+        //this test is based on the assumption that both entries are added at roughly the same time, so we want predictable timing behavior
+        TimeStorage.getInstance().setStoreEntriesAsynch(false);
         val messages = prepareClient(TimeStorage.MQTT_TOPIC);
         String storyName1 = "story1";
         String storyName2 = "story2";
