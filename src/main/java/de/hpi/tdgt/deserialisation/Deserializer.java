297d468c7ef2c703af86f253517a6e9260a7b3b4
Eric Ackermann <eric.ackermann@student.hpi.uni-potsdam.de>
2020-01-20 10:48:21 +0100

    Revert "Remove code that produces nullpointer."
    This reverts commit 8fbca74e54ec527af50c7d07f664043c4bfad524.

--- src/main/java/de/hpi/tdgt/deserialisation/Deserializer.java
+++ src/main/java/de/hpi/tdgt/deserialisation/Deserializer.java
@@ -2,12 +2,15 @@ package de.hpi.tdgt.deserialisation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.hpi.tdgt.test.Test;
+import lombok.val;
 
 import java.io.IOException;
 
 public class Deserializer {
     public static Test deserialize(String json) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(json, Test.class);
+        val test = mapper.readValue(json, Test.class);
+        test.setConfigJSON(json);
+        return test;
     }
 }
