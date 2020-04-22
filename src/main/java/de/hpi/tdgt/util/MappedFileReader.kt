340370fb668ff4016cfa3b30d3a6f60fd7ce18f6
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-04-22 14:43:57 +0200

    Map small portions of a file

--- src/main/java/de/hpi/tdgt/util/MappedFileReader.kt
+++ src/main/java/de/hpi/tdgt/util/MappedFileReader.kt
@@ -17,7 +17,7 @@ class MappedFileReader//e.g. file not found//async reading taken from http://www
     private lateinit var channel: AsynchronousFileChannel
     private lateinit var buffer: ByteBuffer
     private var mappedData:ByteArray = ByteArray(0)
-    private var offset = 0
+    private var offset = 0L
     private var closed = false
 
     var exception: Exception? = null
@@ -27,7 +27,7 @@ class MappedFileReader//e.g. file not found//async reading taken from http://www
             //conversion taken from https://stackoverflow.com/a/28454743, handler from http://tutorials.jenkov.com/java-nio/asynchronousfilechannel.html
             val completableFuture = CompletableFuture<ByteBuffer>()
 
-            channel.read(buffer, 0, buffer, object : CompletionHandler<Int, ByteBuffer> {
+            channel.read(buffer, offset, buffer, object : CompletionHandler<Int, ByteBuffer> {
                 override fun completed(result: Int, attachment: ByteBuffer) {
                     buffer.flip()
                     mappedData = ByteArray(buffer.limit())
@@ -48,7 +48,9 @@ class MappedFileReader//e.g. file not found//async reading taken from http://www
         }
     }
     private val mutex = Semaphore(1)
+    private var currentBufferOffset = 0;
     suspend fun nextLine(): String? {
+
         if(closed){
             return null
         }
@@ -57,43 +59,55 @@ class MappedFileReader//e.g. file not found//async reading taken from http://www
             readData()
         }
         val line = StringBuilder()
-        if(offset >= mappedData.size){
+        if(offset >= channel.size()){
             close()
             return line.toString()
         }
-        var current = mappedData[offset].toChar()
+        var current = mappedData[currentBufferOffset].toChar()
         //left when line break is detected
         while (true) {
-            if(offset + 1 >= mappedData.size){
+            if(offset + 1 >= channel.size()){
                 close()
                 return line.toString()
             }
-            val next = mappedData[offset+1].toChar()
+            val next = mappedData[currentBufferOffset+1].toChar()
             if (current == '\n' || current == '\r') {
                 //Windows style linebreak, filter next character so it is not mistaken for the next line break
                 if (next == '\n' && current == '\r') {
                     offset++
+                    advanceBuffer()
                 }
                 //clear line break for next run
                 offset ++
+                advanceBuffer()
                 break
             } else {
                 line.append(current)
             }
             offset++
-            if(offset >= mappedData.size){
+            advanceBuffer()
+            if(offset >= channel.size()){
                 close()
                 return line.toString()
             }
-            current = mappedData[offset].toChar()
+            current = mappedData[currentBufferOffset].toChar()
         }
         mutex.release()
         return line.toString()
     }
 
+    private suspend fun advanceBuffer() {
+        currentBufferOffset++
+        //passed mapped file segement --> map next segment
+        if(currentBufferOffset == bufferSizeBytes - 1){
+            currentBufferOffset = 0
+            readData()
+        }
+    }
+
     fun hasNextLine(): Boolean {
         if(closed) return false
-        return  offset <= buffer.limit()
+        return  offset <= channel.size()
     }
 
     fun ioException(): Exception? {
@@ -112,13 +126,16 @@ class MappedFileReader//e.g. file not found//async reading taken from http://www
         private val log = LogManager.getLogger(
                 MappedFileReader::class.java
         )
+        @JvmStatic
+        private val bufferSizeBytes = 8
     }
 
     init {
         try {
             val path = Paths.get(filepath)
             channel = AsynchronousFileChannel.open(path, StandardOpenOption.READ)
-            buffer = ByteBuffer.allocate(channel.size().toInt())
+            //1 mb is almost guaranteed to be available, yet large enough to make sense
+            buffer = ByteBuffer.allocate(bufferSizeBytes)//)1024*1024)
             //e.g. file not found
         } catch (e:Exception){
             log.error("Error creating mappedFileReader: ",e)
