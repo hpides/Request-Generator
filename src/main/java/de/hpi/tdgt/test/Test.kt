64c2ef24f625ad04f79e437df9129447c6fa894c
HB <hendrik.bomhardt@student.hpi.de>
2020-04-25 02:18:37 +0200

    added missing space for test end message

--- src/main/java/de/hpi/tdgt/test/Test.kt
+++ src/main/java/de/hpi/tdgt/test/Test.kt
@@ -186,7 +186,7 @@ class Test {
                 log.info("Test run $i complete!")
             }
             //if there is only this node, the test is over; else, other nodes might still be running
-            val endMessage = (if(nodes == 1L){"testEnd"}else{"nodeEnd $nodeNumber "} +"$testId").toByteArray(StandardCharsets.UTF_8)
+            val endMessage = (if(nodes == 1L){"testEnd "}else{"nodeEnd $nodeNumber "} +"$testId").toByteArray(StandardCharsets.UTF_8)
             try {
                 client!!.publish(
                         MQTT_TOPIC,
