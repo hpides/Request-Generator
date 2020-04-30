2c9cd7088b92c2dccce511d37fe1c81e8ba83017
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-04-30 16:33:18 +0200

    Add K8s scripts

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,10 +1,18 @@
 if [ -z ${tag+x} ];
  then tag="latest";
 fi
-docker build -t requestgenerator:${tag} ../ -f ./Dockerfile
+docker build -t requestgenerator:latest ../ -f ./Dockerfile
 if [ -z ${deploy+x} ];
   then echo "no push";
 else
-  docker tag requestgenerator:${tag} localhost:5000/requestgenerator:${tag}
+  docker tag requestgenerator:latest localhost:5000/requestgenerator:${tag}
   docker push localhost:5000/requestgenerator:${tag}
 fi
+docker build -t mosquitto:latest ./ -f ./mosquitto_dockerfile
+if [ -z ${deploy+x} ];
+  then echo "no push";
+else
+  docker tag mosquitto:latest localhost:5000/mosquitto:${tag}
+  docker push localhost:5000/mosquitto:${tag}
+fi
+
---
+++ K8s/deploy.sh
@@ -0,0 +1,10 @@
+cd ../Docker
+export tag=$(date +%s)
+export deploy=true
+export NODENAME=$(kubectl get nodes --no-headers | awk '{ print $1 }' | head -1)
+./build.sh
+cd ../K8s
+for f in manifests/*
+do
+  envsubst < $f | kubectl -n reqgen apply -f -
+done
