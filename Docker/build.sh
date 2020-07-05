40d7d8b02a74ea7fde840ed3d50e0563514bc9fe
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-07-05 12:14:55 +0200

    Allow different docker registries and Docker Hub to be used.

--- Docker/build.sh
+++ Docker/build.sh
@@ -21,18 +21,18 @@
 if [ -z ${tag+x} ];
  then tag="latest";
 fi
-docker build -t requestgenerator:latest ../ -f ./Dockerfile
+docker build -t worldofjarcraft/requestgenerator:latest ../ -f ./Dockerfile
 if [ -z ${deploy+x} ];
   then echo "no push";
 else
-  docker tag requestgenerator:latest localhost:5000/requestgenerator:${tag}
-  docker push localhost:5000/requestgenerator:${tag}
+  docker tag worldofjarcraft/requestgenerator:latest ${REGISTRY}/requestgenerator:${tag}
+  docker push ${REGISTRY}/requestgenerator:${tag}
 fi
-docker build -t mosquitto:latest ./ -f ./mosquitto_dockerfile
+docker build -t  worldofjarcraft/mosquitto:latest ./ -f ./mosquitto_dockerfile
 if [ -z ${deploy+x} ];
   then echo "no push";
 else
-  docker tag mosquitto:latest localhost:5000/mosquitto:${tag}
-  docker push localhost:5000/mosquitto:${tag}
+  docker tag  worldofjarcraft/mosquitto:latest ${REGISTRY}/mosquitto:${tag}
+  docker push ${REGISTRY}/mosquitto:${tag}
 fi
 
--- K8s/deploy.sh
+++ K8s/deploy.sh
@@ -21,8 +21,23 @@
 cd ../Docker
 export tag=$(date +%s)
 export deploy=true
+if [ -z ${REGISTRY+x} ];
+  then export REGISTRY=localhost:5000;
+fi
+printf "Using docker registry $REGISTRY"
+export image=${REGISTRY}/requestgenerator
+export mosquitto_image=${REGISTRY}/mosquitto
 export NODENAME=$(kubectl get nodes --no-headers | awk '{ print $1 }' | head -1)
-./build.sh
+# if not building from source, pull existing image from docker hub
+if [ -z ${DEV+x} ];
+  then
+        export image=worldofjarcraft/requestgenerator;
+	export mosquitto_image=worldofjarcraft/mosquitto;
+        export tag=latest;
+else
+  ./build.sh;
+fi
+echo $image
 cd ../K8s
 for f in manifests/*
 do
